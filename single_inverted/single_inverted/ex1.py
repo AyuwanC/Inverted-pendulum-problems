83a7e7cf4fbb8139d1dcf1a4e7dccb6ab895b440
AyuwanC <ayuwanchudasama@gmail.com>
2024-05-24 13:25:22 +0530

    swing up

--- single_inverted/single_inverted/ex1.py
+++ single_inverted/single_inverted/ex1.py
@@ -1,9 +1,11 @@
-from numpy import pi, tan
+from numpy import pi, cos
 import time
 import rclpy
 from rclpy.time import Time
 from rclpy.node import Node
 from custom_msgs.msg import States, TorqueInput
+
+
 def limited(x, value):
     if x > value:
         return value
@@ -17,7 +19,8 @@ class controller(Node):
     aim = pi
     eprev = 0
     I = 0
-    kp, ki, kd = 100, 1, 1
+    kp, ki, kd = 200, 1, 1
+    flag = False
     def __init__(self):
         super().__init__('controller')
         self.subscription = self.create_subscription(
@@ -31,27 +34,50 @@ class controller(Node):
             "/torque_input",
             10
         )
+    def currEnergy(theta, omega):
+        pot = 9.81 * (1 - cos(theta))
+        kin = 0.5 * omega * omega
+        return pot + kin
+    
     def callback(self, msgin:States):
         msgout = TorqueInput()
 
         # PID
-        tdiff = 1/500
+        if self.flag:
+            tdiff = 1/500
 
-        if msgin.theta > 0:
-            enow = self.aim - abs(msgin.theta)
+            if msgin.theta > 0:
+                enow = self.aim - abs(msgin.theta)
+            else:
+                enow = -(msgin.theta + self.aim)
+            
+            P = self.kp * enow
+
+            D = self.kd * (enow - self.eprev)/tdiff
+            print(enow)
+            torque = P + self.I + D
+            self.I += self.ki * enow
+            torque = limited(torque, 5)
+
+            self.eprev = enow
+            msgout.torque_value = torque * 1.0
+        #/PID
         else:
-            enow = -(msgin.theta + self.aim)
-        
-        P = self.kp * enow
-
-        D = self.kd * (enow - self.eprev)/tdiff
-        print(enow)
-        torque = P + self.I + D
-        self.I += self.ki * enow
-        torque = limited(torque, 20)
-
-        self.eprev = enow
-        msgout.torque_value = torque *1.0
+            omega = msgin.theta_dot
+            pot = 9.81 * (1 - cos(msgin.theta))
+            kin = 0.5 * omega * omega
+            energy = pot + kin
+            if ((-pi <= msgin.theta <= -0.88*pi) and msgin.theta_dot >= 0) or ((0.88*pi <= msgin.theta <= pi) and msgin.theta_dot <=0):
+                self.flag = True
+                msgout.torque_value = 0.0
+            else:
+                if 0 <= msgin.theta < pi/4 and msgin.theta_dot >= 0:
+                    msgout.torque_value = 4.0
+                elif -pi/4 <= msgin.theta < 0 and msgin.theta_dot <= 0:
+                    msgout.torque_value = -4.0
+                else: 
+                    msgout.torque_value = 0.0
+       
         self.torque.publish(msgout)
 
 def main(args = None):
